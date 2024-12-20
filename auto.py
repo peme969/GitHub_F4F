39ad09ac074e38d05921bcd7e9c39402157c02a9
Peme969 ⚡ <mrcoderpeme@gmail.com>
2024-12-19 19:22:22 -0600

    Update auto.py

--- auto.py
+++ auto.py
@@ -73,13 +73,17 @@ following = [user['login'].lower() for user in following_data]
 followers = [user['login'].lower() for user in followers_data] 
 not_followers = [user for user in following if user not in followers]
 not_following_back = [user for user in followers if user not in following]
-for user in not_followers:
-    print(f'\033[1;33mYou follow {user}, but they don\'t follow you back.\033[0m')
-    unfollow_user(user)
-for user in not_following_back:
-    print(f'\033[1;36m{user} follows you, but you don\'t follow them back.\033[0m')
-    follow_user(user)
-print("\033[1;34mProcessing complete!\033[0m")
-print(f'\033[1;34m📊 Summary:\033[0m')
-print(f'\033[1;33m🔴 {len(not_followers)} user(s) you follow but don\'t follow you back.\033[0m')
-print(f'\033[1;36m🟢 {len(not_following_back)} user(s) follow you but you don\'t follow back.\033[0m')
+if os.environ['Consent'].lower() != 'yes' or os.environ['Consent'] != 'y':
+    print('\033[31;1mSorry, it seems like you havent provided consent to automate following and unfollowing users... Please change this by entering Yes or Y (not case sensitive) in the Action Secrets.\033[0m')
+else:
+    print('\033[37;1mConsent provided! Continuing WorkFlow\033[0m')
+    for user in not_followers:
+        print(f'\033[1;33mYou follow {user}, but they don\'t follow you back.\033[0m')
+        unfollow_user(user)
+    for user in not_following_back:
+        print(f'\033[1;36m{user} follows you, but you don\'t follow them back.\033[0m')
+        follow_user(user)
+    print("\033[1;34mProcessing complete!\033[0m")
+    print(f'\033[1;34m📊 Summary:\033[0m')
+    print(f'\033[1;33m🔴 {len(not_followers)} user(s) you follow but don\'t follow you back.\033[0m')
+    print(f'\033[1;36m🟢 {len(not_following_back)} user(s) follow you but you don\'t follow back.\033[0m')
